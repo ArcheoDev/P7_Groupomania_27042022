1ab4bc7f19a08926c503c1d87273c6f41dac63d7
ArchéoDev <helene.labit@gmail.com>
2022-04-27 12:16:53 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+// Importation
+
+require("dotenv").config();
+// const express = require("express");
+// const path = require("path");
+
+  
+//Express
+
+const app = express();
+app.use(express.json());
+
+//Correction erreur CORS
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', `${process.env.CLIENT_URL}`);
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+
+module.exports = app;
